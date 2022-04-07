db9808191b301a57f62c1eeb150c14bbbbd6f228
Humberto Arndt <humberto.arndt@gmail.com>
2022-04-07 09:56:37 -0400

    Add libc functions

---
+++ ft_atoi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 14:04:33 by harndt            #+#    #+#             */
+/*   Updated: 2022/04/07 09:52:26 by harndt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (ft_isdigit(*str))
+	{
+		res = res * 10 + *str - '0';
+		str++;
+	}
+	return (res * sign);
+}
---
+++ ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 14:43:31 by harndt            #+#    #+#             */
+/*   Updated: 2022/04/07 09:52:49 by harndt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	void	*ret;
+
+	ret = malloc(nitems * size);
+	if (ret)
+		ft_fbzero(ret, nitems * size);
+	return (ret);
+}
