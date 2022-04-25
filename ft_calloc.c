a886c05c69680b838eab061d94991e0f257ba793
Humberto Arndt <humberto.arndt@gmail.com>
2022-04-25 16:10:44 -0400

    Update Libc Functions (Mandatory)

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 14:43:31 by harndt            #+#    #+#             */
-/*   Updated: 2022/04/21 17:16:51 by harndt           ###   ########.fr       */
+/*   Updated: 2022/04/25 16:03:25 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	*ft_calloc(size_t nitems, size_t size)
 {
 	void	*ret;
 
+	if (nitems == 0 || size == 0)
+		return (NULL);
+	if (2147483647 / nitems < size)
+		return (NULL);
 	ret = malloc(nitems * size);
 	if (ret)
 		ft_bzero(ret, nitems * size);
--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 19:18:23 by harndt            #+#    #+#             */
-/*   Updated: 2022/04/21 17:18:30 by harndt           ###   ########.fr       */
+/*   Updated: 2022/04/25 15:56:30 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, int n)
+void	*ft_memset(void *str, int c, size_t n)
 {
 	unsigned char	*ptr;
 
