5418df326fd7946644d349c124d1a743c82029d4
harndt <harndt@student.42sp.org.br>
2022-07-19 20:25:32 +0200

    Fix NULL security in ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: harndt <humberto.arndt@gmail.com>          +#+  +:+       +#+        */
+/*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:10:51 by harndt            #+#    #+#             */
-/*   Updated: 2022/04/25 15:57:05 by harndt           ###   ########.fr       */
+/*   Updated: 2022/07/19 20:24:44 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *str, int c)
 {
+	if (!str)
+		return (NULL);
 	while (*str)
 		if (*str++ == (char)c)
 			return ((char *)(str - 1));
